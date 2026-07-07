7d2921a3e1c427a0c80fbcb5e132e3287a726f05
Isaac Mulatya <imulatya30@gmail.com>
2026-07-07 18:06:02 +0300

    package management pipeline initialized with package.json and custom dependencies

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
 const os = require('os');
+const chalk = require('chalk');
 
-console.log("---------------------------------------");
-console.log("Node.js Runtime Engine Initialized!");
-console.log("---------------------------------------");
+console.log(chalk.blue.bold("---------------------------------------"));
+console.log(chalk.green.bold("Node.js Runtime Engine Initialized!"));
+console.log(chalk.blue.bold("---------------------------------------"));
+
 const totalMemoryGB = (os.totalmem() / 1024 / 1024 / 1024).toFixed(2);
 const freeMemoryGB = (os.freemem() / 1024 / 1024 / 1024).toFixed(2);
 
-console.log(`OS Platform: ${os.platform()}`);
-console.log(` CPU Architecture: ${os.arch()}`);
-console.log(`Total System Memory: ${totalMemoryGB} GB`);
-console.log(`Free System Memory: ${freeMemoryGB} GB`);
-console.log("---------------------------------------");
+console.log(`${chalk.yellow("OS Platform:")} ${os.platform()}`);
+console.log(`${chalk.yellow("CPU Architecture:")} ${os.arch()}`);
+console.log(`${chalk.cyan("Total System Memory:")} ${totalMemoryGB} GB`);
+console.log(`${chalk.cyan(" Free System Memory:")} ${freeMemoryGB} GB`);
+console.log(chalk.blue.bold("---------------------------------------"));
